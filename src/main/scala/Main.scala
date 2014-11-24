f7c03e28bb2b6b14067b09ae2cae35986770b3ff
iwag <iwagame@gmail.com>
2014-11-25 00:53:50 +0900

    use filter

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,19 +3,71 @@ package com.github.iwag
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.filter.LogFormatter
+import com.twitter.finagle.filter.LoggingFilter
+import com.twitter.finagle.service.{RetryPolicy, RetryingFilter}
 import com.twitter.finagle.thrift.ThriftClientFramedCodec
 import com.twitter.server.TwitterServer
-import com.twitter.finagle.{Thrift, Service, Http}
+import com.twitter.finagle.{SimpleFilter, Thrift, Service, Http}
 import com.twitter.finagle.http.HttpMuxer
 import com.twitter.io.Charsets._
 import com.twitter.logging.Logger
-import com.twitter.util.{Time, Future, Await}
+import com.twitter.util._
 import org.apache.thrift.protocol.TBinaryProtocol
 import org.jboss.netty.buffer.ChannelBuffers._
 import org.jboss.netty.handler.codec.http._
 import org.elasticsearch.thrift._
 
-class HTTPServerImpl(log:Logger, esAddr : InetSocketAddress, cacheAddr: InetSocketAddress) extends Service[HttpRequest, HttpResponse]{
+class CacheFilter(cacheAddr: InetSocketAddress) extends SimpleFilter[HttpRequest, HttpResponse] {
+  private[this] lazy val cacheClient = Thrift.client.withProtocolFactory(new TBinaryProtocol.Factory()).newIface[CacheService.FutureIface](cacheAddr.toString) // FIXME
+
+  override def apply(request: HttpRequest, service: Service[HttpRequest, HttpResponse]): Future[HttpResponse] = {
+    cacheClient.get(request.getUri) flatMap {
+      _.hit match {
+        case Some(v) => {
+          val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+          response.setContent(copiedBuffer(v, Utf8))
+          Future(response)
+        }
+        case _ => service(request)
+      }
+    }
+  }
+}
+
+class ESService(esAddr: InetSocketAddress) extends Service[HttpRequest, HttpResponse] {
+
+  private[this] lazy val esClient =
+    Thrift.client.withProtocolFactory(new TBinaryProtocol.Factory()).newIface[Rest.FutureIface](esAddr.toString) // FIXME
+
+  override def apply(request: HttpRequest): Future[HttpResponse] = {
+    val es = esClient.execute(RestRequest(Method.Post, "/user/item/_search", None, None, Some(ByteBuffer.wrap("test".getBytes))))
+    es map { res =>
+      val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+      response.setContent(copiedBuffer(res.body.get))
+      response
+    }
+  }
+}
+
+class HTTPLoggingFilter(l: Logger) extends LoggingFilter[HttpRequest, HttpResponse] {
+  override val log: Logger = l
+  override val formatter: LogFormatter[HttpRequest, HttpResponse] = new LogFormatter[HttpRequest, HttpResponse] {
+    override def format(request: HttpRequest, reply: HttpResponse, replyTime: Duration): String = ???
+
+    override def formatException(request: HttpRequest, throwable: Throwable, replyTime: Duration): String = ???
+  }
+}
+
+class HTTPServerImpl(log: Logger, esAddr: InetSocketAddress, cacheAddr: InetSocketAddress) extends Service[HttpRequest, HttpResponse] {
+
+  val cacheFilter = new CacheFilter(cacheAddr)
+  val esService = new ESService(esAddr)
+
+  val retryFilter = new RetryingFilter[HttpRequest, HttpResponse](RetryPolicy.tries(2), new MockTimer)
+  val logFilter = new HTTPLoggingFilter(log)
+
+  val comp = logFilter andThen cacheFilter andThen retryFilter andThen esService
 
   private[this] lazy val cacheClient = {
     val s = ClientBuilder().hosts(cacheAddr)
@@ -26,25 +78,7 @@ class HTTPServerImpl(log:Logger, esAddr : InetSocketAddress, cacheAddr: InetSock
   private[this] lazy val esClient =
     Thrift.client.withProtocolFactory(new TBinaryProtocol.Factory()).newIface[Rest.FutureIface](esAddr.toString) // FIXME
 
-  override def apply(request: HttpRequest): Future[HttpResponse] = {
-    log.info(s"Received at ${Time.now} ${request}")
-
-    val cacheResp:Future[CacheResponse] = cacheClient.get(request.getUri)
-
-    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-
-    cacheResp.flatMap(res => res.hit match {
-      case Some(v) => Future(v)
-      case _ => {
-        val es = esClient.execute(RestRequest(Method.Post, "/user/item/_search", None, None, Some(ByteBuffer.wrap("test".getBytes))))
-        es map (_.body.get.asCharBuffer().toString)
-      }
-    }).map { str =>
-      val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-      response.setContent(copiedBuffer(str, Utf8))
-      response
-    }
-  }
+  override def apply(request: HttpRequest): Future[HttpResponse] = comp(request)
 }
 
 object HTTPServer extends TwitterServer {
