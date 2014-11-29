63f43321393e6169d53f7df4d5373d10f19ebcd9
iwag <iwagame@gmail.com>
2014-11-29 19:59:20 +0900

    thrift service

--- src/main/scala/SearchServer.scala
+++ src/main/scala/SearchServer.scala
@@ -3,7 +3,7 @@ package com.github.iwag
 import java.net.InetSocketAddress
 import java.util.concurrent.atomic.AtomicInteger
 
-import com.twitter.finagle.Thrift
+import com.twitter.finagle.{ListeningServer, ServiceFactory, Service, Thrift}
 import com.twitter.logging.Logger
 import com.twitter.server.TwitterServer
 import com.twitter.util.{Await, Future}
@@ -11,56 +11,3 @@ import org.apache.thrift.protocol.TBinaryProtocol
 
 import scala.collection.mutable
 
-class SearchServerImpl(log:Logger) extends SearchService.FutureIface {
-
-  val index = new AtomicInteger(0)
-
-  val table = new mutable.HashMap[String, mutable.Set[Int]] with mutable.MultiMap[String, Int]
-
-  def calcBigram(s: String): Set[String] = {
-    if (s.length < 2) Set()
-    else Range(0, s.length - 1).toList.map(v=>s(v).toString + s(v + 1)).toSet
-  }
-
-  override def put(value: String): Future[Int] = Future.value {
-    val idx = index.incrementAndGet()
-    calcBigram(value) foreach (s => table.getOrElseUpdate(s, mutable.Set[Int]()) += idx)
-
-    log.info(s"put ${value} as ${idx}")
-    idx
-  }
-
-  override def search(key: String): Future[SearchResponse] = Future.value{
-    val bigramed = calcBigram(key)
-    val foundIdxs = bigramed flatMap (table.get(_))
-    val all = foundIdxs reduceLeft (_ & _)
-
-    log.info(s"hit ${all} by ${key}")
-    SearchResponse(all.toSeq, "OK")
-  }
-
-  override def delete(id: Int):Future[Unit] = Future.value {
-    log.info(s"delete ${id}")
-    table foreach { kv => if (kv._2.contains(id)) kv._2.remove(id)}
-  }
-}
-
-object SearchServer extends TwitterServer {
-  val thriftAddr = flag("bind", new InetSocketAddress(9090), "bind address")
-
-  val service = new SearchService.FinagledService(new SearchServerImpl(log), new TBinaryProtocol.Factory())
-
-  val server = Thrift.server.serve(thriftAddr(), service)
-
-  def main() {
-
-    onExit {
-      adminHttpServer.close()
-      server.close()
-    }
-
-    log.info("start admin:"+adminHttpServer.boundAddress)
-    log.info("start thrift:"+server.boundAddress)
-    Await.all(adminHttpServer, server)
-  }
-}
--- src/main/scala/ServiceImpl.scala
+++ src/main/scala/ServiceImpl.scala
@@ -1,10 +1,15 @@
 package com.github.iwag
 
+import java.net.InetSocketAddress
+import java.util.concurrent.atomic.AtomicInteger
+
 import com.twitter.server.TwitterServer
-import com.twitter.finagle.Thrift
+import com.twitter.finagle.{Service, Thrift}
 import com.twitter.util.{Future, Await}
 import com.twitter.logging.Logger
 import org.apache.thrift.protocol.TBinaryProtocol
+
+import scala.collection.mutable
 
 class CacheServerImpl(log:Logger) extends CacheService.FutureIface {
   val ExpireSeconds = 10
@@ -33,22 +38,70 @@ class CacheServerImpl(log:Logger) extends CacheService.FutureIface {
   }
 }
 
+class SearchServerImpl(log:Logger) extends SearchService.FutureIface {
 
-object CacheServer extends TwitterServer {
+  val index = new AtomicInteger(0)
 
-  val service = new CacheService.FinagledService(new CacheServerImpl(log), new TBinaryProtocol.Factory())
+  val table = new mutable.HashMap[String, mutable.Set[Int]] with mutable.MultiMap[String, Int]
 
-  val server = Thrift.server.serve(":49090", service)
+  def calcBigram(s: String): Set[String] = {
+    if (s.length < 2) Set()
+    else Range(0, s.length - 1).toList.map(v=>s(v).toString + s(v + 1)).toSet
+  }
 
-  def main() {
+  override def put(value: String): Future[Int] = Future.value {
+    val idx = index.incrementAndGet()
+    calcBigram(value) foreach (s => table.getOrElseUpdate(s, mutable.Set[Int]()) += idx)
+
+    log.info(s"put ${value} as ${idx}")
+    idx
+  }
+
+  override def search(key: String): Future[SearchResponse] = Future.value{
+    val bigramed = calcBigram(key)
+    val foundIdxs = bigramed flatMap (table.get(_))
+    val all = foundIdxs reduceLeft (_ & _)
+
+    log.info(s"hit ${all} by ${key}")
+    SearchResponse(all.toSeq, "OK")
+  }
+
+  override def delete(id: Int):Future[Unit] = Future.value {
+    log.info(s"delete ${id}")
+    table foreach { kv => if (kv._2.contains(id)) kv._2.remove(id)}
+  }
+}
+
+trait ThriftServer extends TwitterServer {
+  val thriftAddr = flag("bind", new InetSocketAddress(9090), "bind address")
+
+  def start( service: Service[Array[Byte], Array[Byte]]
+             ) {
+    val server = Thrift.server.serve(thriftAddr(), service)
 
     onExit {
       adminHttpServer.close()
       server.close()
     }
 
-    log.info("start admin:"+adminHttpServer.boundAddress)
-    log.info("start thrift:"+server.boundAddress)
+    log.info("start admin:" + adminHttpServer.boundAddress)
+    log.info("start thrift:" + server.boundAddress)
     Await.all(adminHttpServer, server)
+  }
+}
+
+object SearchServer extends TwitterServer with ThriftServer {
+  def main() = {
+    val service = new SearchService.FinagledService(new SearchServerImpl(log), new TBinaryProtocol.Factory())
+
+    start(service)
+  }
+}
+
+object CacheServer extends TwitterServer with ThriftServer {
+  def main() = {
+    val service = new CacheService.FinagledService(new CacheServerImpl(log), new TBinaryProtocol.Factory())
+
+    start(service)
   }
 }
